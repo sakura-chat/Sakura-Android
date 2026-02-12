807948e16657f8d71b269ea761290b5648716863
Kayra Uylar <k.uylar@outlook.com>
2026-02-12 19:51:23 +0300

    feat: add a bottomnav view

--- app/src/main/java/dev/kuylar/sakura/ui/activity/MainActivity.kt
+++ app/src/main/java/dev/kuylar/sakura/ui/activity/MainActivity.kt
@@ -1,6 +1,7 @@
 package dev.kuylar.sakura.ui.activity
 
 import android.Manifest
+import android.animation.ValueAnimator
 import android.app.ComponentCaller
 import android.app.NotificationManager
 import android.content.Intent
@@ -10,18 +11,29 @@ import android.provider.Settings
 import android.provider.Settings.ACTION_APP_NOTIFICATION_SETTINGS
 import android.util.Log
 import android.util.TypedValue
+import android.view.MenuItem
 import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
 import androidx.core.content.edit
 import androidx.core.content.getSystemService
+import androidx.core.graphics.drawable.toDrawable
 import androidx.core.os.bundleOf
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.drawToBitmap
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
 import androidx.core.view.postDelayed
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.NavController
 import androidx.navigation.NavOptions
 import androidx.navigation.fragment.NavHostFragment
@@ -29,7 +41,9 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.discord.panels.OverlappingPanelsLayout
 import com.discord.panels.PanelState
 import com.discord.panels.PanelsChildGestureRegionObserver
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.navigation.NavigationBarView
 import com.google.firebase.messaging.FirebaseMessaging
 import dagger.hilt.android.AndroidEntryPoint
 import de.connect2x.trixnity.client.store.Room
@@ -53,10 +67,12 @@ import com.google.android.material.R as MaterialR
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(), PanelsChildGestureRegionObserver.GestureRegionsListener,
-	OverlappingPanelsLayout.PanelStateListener {
+	OverlappingPanelsLayout.PanelStateListener,
+	NavigationBarView.OnItemSelectedListener {
 	@Inject
 	lateinit var client: Matrix
 	private lateinit var binding: ActivityMainBinding
+	private lateinit var navHostFragment: NavHostFragment
 	private lateinit var navController: NavController
 	private var autoNavigate = true
 	private val notificationPermissionLauncher =
@@ -65,6 +81,7 @@ class MainActivity : AppCompatActivity(), PanelsChildGestureRegionObserver.Gestu
 				showNotificationPermissionRationale(shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS))
 			}
 		}
+	private var startPanelState: PanelState = PanelState.Closed
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -75,12 +92,14 @@ class MainActivity : AppCompatActivity(), PanelsChildGestureRegionObserver.Gestu
 
 		if (!Matrix.isInitialized()) Matrix.setClient(client)
 
+		navHostFragment =
+			supportFragmentManager.findFragmentById(binding.navHostFragment.id) as NavHostFragment
+		navController = navHostFragment.navController
+
 		ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
 			val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
 			val ime = insets.getInsets(WindowInsetsCompat.Type.ime())
 
-			val navHostFragment =
-				supportFragmentManager.findFragmentById(binding.navHostFragment.id) as NavHostFragment
 			navHostFragment.childFragmentManager.fragments.forEach { fragment ->
 				if (fragment is TimelineFragment) {
 					fragment.onImeHeightChanged(ime.bottom)
@@ -97,10 +116,6 @@ class MainActivity : AppCompatActivity(), PanelsChildGestureRegionObserver.Gestu
 			insets
 		}
 
-		val navHostFragment =
-			supportFragmentManager.findFragmentById(binding.navHostFragment.id) as NavHostFragment
-		navController = navHostFragment.navController
-
 		setSupportActionBar(binding.toolbar)
 		binding.toolbar.setNavigationOnClickListener {
 			binding.overlappingPanels.openStartPanel()
@@ -115,6 +130,7 @@ class MainActivity : AppCompatActivity(), PanelsChildGestureRegionObserver.Gestu
 				else -> false
 			}
 		}
+		binding.bottomNav.setOnItemSelectedListener(this)
 
 		PanelsChildGestureRegionObserver.Provider.get().addGestureRegionsUpdateListener(this)
 
@@ -139,7 +155,29 @@ class MainActivity : AppCompatActivity(), PanelsChildGestureRegionObserver.Gestu
 		binding.roomsPanel.roomsRecycler.layoutManager = LinearLayoutManager(this)
 		binding.overlappingPanels.registerStartPanelStateListeners(this)
 
+		binding.bottomNav.post {
+			binding.bottomNav.hide()
+		}
 		handleStateChange(SyncState.STOPPED)
+		lifecycleScope.launch {
+			lifecycle.repeatOnLifecycle(Lifecycle.State.RESUMED) {
+				navController.addOnDestinationChangedListener { _, destination, _ ->
+					when (destination.id) {
+						R.id.nav_empty -> {
+							binding.overlappingPanels.openStartPanel()
+							binding.overlappingPanels.setStartPanelUseFullPortraitWidth(true)
+							binding.overlappingPanels.setStartPanelLockState(OverlappingPanelsLayout.LockState.OPEN)
+						}
+
+						R.id.nav_room -> {
+							binding.overlappingPanels.closePanels()
+							binding.overlappingPanels.setStartPanelUseFullPortraitWidth(false)
+							binding.overlappingPanels.setStartPanelLockState(OverlappingPanelsLayout.LockState.UNLOCKED)
+						}
+					}
+				}
+			}
+		}
 		suspendThread {
 			try {
 				client.initialize("main")
@@ -329,7 +367,7 @@ class MainActivity : AppCompatActivity(), PanelsChildGestureRegionObserver.Gestu
 		return if (intent.action == Intent.ACTION_VIEW) {
 			val uri = intent.data ?: return false
 
-			return when (uri.host) {
+			when (uri.host) {
 				"room" -> {
 					uri.lastPathSegment?.let { roomId ->
 						openRoomTimeline(roomId)
@@ -354,12 +392,113 @@ class MainActivity : AppCompatActivity(), PanelsChildGestureRegionObserver.Gestu
 	}
 
 	override fun onPanelStateChange(panelState: PanelState) {
-		val navHostFragment =
-			supportFragmentManager.findFragmentById(binding.navHostFragment.id) as NavHostFragment
+		if (startPanelState == panelState) return
+
+		when (panelState) {
+			PanelState.Closing -> {
+				if (getCurrentRoomId() != null)
+					binding.bottomNav.hide()
+			}
+
+			PanelState.Opening -> {
+				binding.bottomNav.show()
+			}
+
+			else -> {}
+		}
+
+		startPanelState = panelState
 		navHostFragment.childFragmentManager.fragments.forEach { fragment ->
 			if (fragment is TimelineFragment) {
 				fragment.closeKeyboard()
 			}
+		}
+	}
+
+	override fun onNavigationItemSelected(item: MenuItem): Boolean {
+		return when (item.itemId) {
+			R.id.nav_main -> {
+				navController.popBackStack(R.id.nav_room, false)
+			}
+
+			R.id.nav_search -> {
+				Toast.makeText(this, "not implemented yet", Toast.LENGTH_LONG).show()
+				false
+			}
+
+			R.id.nav_notifications -> {
+				Toast.makeText(this, "not implemented yet", Toast.LENGTH_LONG).show()
+				false
+			}
+
+			R.id.nav_settings -> {
+				Toast.makeText(this, "not implemented yet", Toast.LENGTH_LONG).show()
+				false
+			}
+
+			else -> {
+				false
+			}
+		}
+	}
+
+	private fun BottomNavigationView.show() {
+		if (isVisible) return
+
+		val parent = parent as ViewGroup
+		if (!isLaidOut) {
+			measure(
+				View.MeasureSpec.makeMeasureSpec(parent.width, View.MeasureSpec.EXACTLY),
+				View.MeasureSpec.makeMeasureSpec(parent.height, View.MeasureSpec.AT_MOST)
+			)
+			layout(parent.left, parent.height - measuredHeight, parent.right, parent.height)
+		}
+
+		val drawable = drawToBitmap().toDrawable(context.resources)
+		drawable.setBounds(left, parent.height, right, parent.height + height)
+		parent.overlay.add(drawable)
+		ValueAnimator.ofInt(parent.height, top).apply {
+			startDelay = 100L
+			duration = 300L
+			interpolator = AnimationUtils.loadInterpolator(
+				context,
+				android.R.interpolator.linear_out_slow_in
+			)
+			addUpdateListener {
+				val newTop = it.animatedValue as Int
+				drawable.setBounds(left, newTop, right, newTop + height)
+			}
+			doOnEnd {
+				parent.overlay.remove(drawable)
+				visibility = View.VISIBLE
+			}
+			start()
+		}
+	}
+
+	private fun BottomNavigationView.hide() {
+		if (isGone) return
+
+		val drawable = drawToBitmap().toDrawable(context.resources)
+		val parent = parent as ViewGroup
+		drawable.setBounds(left, top, right, bottom)
+		parent.overlay.add(drawable)
+		visibility = View.GONE
+		ValueAnimator.ofInt(top, parent.height).apply {
+			startDelay = 100L
+			duration = 200L
+			interpolator = AnimationUtils.loadInterpolator(
+				context,
+				android.R.interpolator.fast_out_linear_in
+			)
+			addUpdateListener {
+				val newTop = it.animatedValue as Int
+				drawable.setBounds(left, newTop, right, newTop + height)
+			}
+			doOnEnd {
+				parent.overlay.remove(drawable)
+			}
+			start()
 		}
 	}
 }
