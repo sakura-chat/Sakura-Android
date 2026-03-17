621870b6ed850f019da5c3e261974830f21be46b
Kayra Uylar <k.uylar@outlook.com>
2026-03-17 17:30:14 +0300

    fix: correctly display custom emoji in eventbottomsheetfragment

--- app/src/main/java/dev/kuylar/sakura/ui/fragment/bottomsheet/EventBottomSheetFragment.kt
+++ app/src/main/java/dev/kuylar/sakura/ui/fragment/bottomsheet/EventBottomSheetFragment.kt
@@ -3,13 +3,21 @@ package dev.kuylar.sakura.ui.fragment.bottomsheet
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.text.SpannableStringBuilder
+import android.text.Spanned
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.core.view.postDelayed
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
 import de.connect2x.trixnity.client.room
@@ -20,6 +28,8 @@ import de.connect2x.trixnity.client.store.sender
 import de.connect2x.trixnity.client.user
 import de.connect2x.trixnity.core.model.EventId
 import de.connect2x.trixnity.core.model.RoomId
+import dev.kuylar.mentionsedittext.ImageMentionSpan
+import dev.kuylar.sakura.R
 import dev.kuylar.sakura.Utils.suspendThread
 import dev.kuylar.sakura.client.Matrix
 import dev.kuylar.sakura.client.customevent.RecentEmoji
@@ -34,7 +44,8 @@ import de.connect2x.trixnity.core.model.events.m.room.RoomMessageEventContent as
 class EventBottomSheetFragment : BottomSheetDialogFragment() {
 	private lateinit var binding: FragmentEventBottomSheetBinding
 	private lateinit var event: TimelineEvent
-	@Inject lateinit var client: Matrix
+	@Inject
+	lateinit var client: Matrix
 	private var eventType: String? = null
 	private var eventId: EventId? = null
 	private var roomId: RoomId? = null
@@ -179,55 +190,51 @@ class EventBottomSheetFragment : BottomSheetDialogFragment() {
 			binding.root.postDelayed(50) { dismiss() }
 		}
 
-		binding.quickReaction1Text.text = recentEmojis[0].emoji
-		binding.quickReaction2Text.text = recentEmojis[1].emoji
-		binding.quickReaction3Text.text = recentEmojis[2].emoji
-		binding.quickReaction4Text.text = recentEmojis[3].emoji
-		binding.quickReaction5Text.text = recentEmojis[4].emoji
-		binding.quickReaction1.setOnClickListener {
-			suspendThread {
-				client.reactToEvent(roomId!!, eventId!!, recentEmojis[0].emoji)
-			}
-			binding.root.postDelayed(50) {
-				dismiss()
-			}
-		}
-		binding.quickReaction2.setOnClickListener {
-			suspendThread {
-				client.reactToEvent(roomId!!, eventId!!, recentEmojis[1].emoji)
-			}
-			binding.root.postDelayed(50) {
-				dismiss()
-			}
-		}
-		binding.quickReaction3.setOnClickListener {
-			suspendThread {
-				client.reactToEvent(roomId!!, eventId!!, recentEmojis[2].emoji)
-			}
-			binding.root.postDelayed(50) {
-				dismiss()
-			}
-		}
-		binding.quickReaction4.setOnClickListener {
-			suspendThread {
-				client.reactToEvent(roomId!!, eventId!!, recentEmojis[3].emoji)
-			}
-			binding.root.postDelayed(50) {
-				dismiss()
-			}
-		}
-		binding.quickReaction5.setOnClickListener {
-			suspendThread {
-				client.reactToEvent(roomId!!, eventId!!, recentEmojis[4].emoji)
-			}
-			binding.root.postDelayed(50) {
-				dismiss()
-			}
-		}
+		setReaction(binding.quickReaction1, binding.quickReaction1Text, recentEmojis[0].emoji)
+		setReaction(binding.quickReaction2, binding.quickReaction2Text, recentEmojis[1].emoji)
+		setReaction(binding.quickReaction3, binding.quickReaction3Text, recentEmojis[2].emoji)
+		setReaction(binding.quickReaction4, binding.quickReaction4Text, recentEmojis[3].emoji)
+		setReaction(binding.quickReaction5, binding.quickReaction5Text, recentEmojis[4].emoji)
 		binding.reaction.setOnClickListener {
 			val f = ReactionBottomSheetFragment()
 			f.arguments = bundleOf("roomId" to roomId?.full, "eventId" to eventId?.full)
 			f.show(parentFragmentManager, "reactionBottomSheet")
+			binding.root.postDelayed(50) {
+				dismiss()
+			}
+		}
+	}
+
+	private fun setReaction(parent: FrameLayout, text: TextView, emoji: String) {
+		if (emoji.startsWith("mxc://")) {
+			val span = ImageMentionSpan("￼") {
+				Glide.with(requireContext())
+					.asDrawable()
+					.load(emoji)
+					.error(R.drawable.ic_emoji)
+					.into(object : CustomTarget<Drawable>() {
+						override fun onResourceReady(
+							resource: Drawable,
+							transition: Transition<in Drawable>?
+						) {
+							it(resource)
+						}
+
+						override fun onLoadCleared(placeholder: Drawable?) {}
+					})
+			}
+			span.onImageLoaded = {
+				text.text = text.text
+			}
+			val s = SpannableStringBuilder()
+			s.append("￼")
+			s.setSpan(span, 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+			text.text = s
+		} else text.text = emoji
+		parent.setOnClickListener {
+			suspendThread {
+				client.reactToEvent(roomId!!, eventId!!, emoji)
+			}
 			binding.root.postDelayed(50) {
 				dismiss()
 			}
